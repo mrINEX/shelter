93f6394edc12367a0535238a02a93bbc4107539a
mrinex <mrinex@mail.ru>
2020-10-21 23:13:55 +0300

    feat: add slider functionality

---
+++ shelter/pages/pets/script.js
@@ -0,0 +1,5 @@
+// document.querySelector('.header-nav').addEventListener('click', (event) => {
+//   if (event.target.innerHTML !== 'About the shelter') {
+//     event.preventDefault();
+//   }
+// });
